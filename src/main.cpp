aef342bda88a180691b54018eeb22cc16017a5a6
Rudy Bernard <rudybernard1@gmail.com>
2023-01-19 14:27:03 -0500

    to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,110 @@
 #include "bridge.h"
 #include <Arduino.h>
 #include <Spidermesh.h>
+#include <ETH.h>
 
 Spidermesh smk900;
 const int server_serial_port = 5556;
 Bridge bridge(server_serial_port);
 bool prevPollMode;
 
+#define ETH_CLK_MODE ETH_CLOCK_GPIO0_OUT
+// Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
+#define ETH_POWER_PIN 4
+// Type of the Ethernet PHY (LAN8720 or TLK110)
+#define ETH_TYPE ETH_PHY_LAN8720
+// I²C-address of Ethernet PHY (0 or 1 for LAN8720, 31 for TLK110)
+#define ETH_ADDR 0
+// Pin# of the I²C clock signal for the Ethernet PHY
+#define ETH_MDC_PIN 23
+// Pin# of the I²C IO signal for the Ethernet PHY
+#define ETH_MDIO_PIN 18
+
+IPAddress eth_add(192,168,0,30);
+IPAddress eth_gat(192,168,0,1);
+IPAddress eth_msk(255,255,255,0);
+IPAddress eth_gw1(192,168,0,1);
+IPAddress eth_gw2(8,8,8,8);
+
 void WhenPacketReceived(apiframe packet);
+
+
+
+//---------------------------------------------------------------------------------------
+void WiFiEvent(WiFiEvent_t event)
+{
+    static int test = 0;
+    switch (event)
+    {
+    case SYSTEM_EVENT_ETH_START:
+        Serial.println("SYSTEM_EVENT_ETH_START");
+        //set eth hostname here
+        //ETH.setHostname("esp32-ethernet");
+        break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+        Serial.println("ETH Connected");
+        break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+            Serial.println("SYSTEM_EVENT_ETH_GOT_IP");
+            Serial.print("ETH MAC: ");
+            Serial.println(ETH.macAddress());
+            Serial.print("IPv4: ");
+            Serial.println(ETH.localIP());
+            Serial.print("Subnet mask: ");
+            Serial.println(ETH.subnetMask());
+            Serial.print("Gateway IP: ");
+            Serial.println(ETH.gatewayIP());
+            if (ETH.fullDuplex())
+            {
+                Serial.println("FULL_DUPLEX");
+            }
+            Serial.print(ETH.linkSpeed());
+            Serial.println("Mbps");
+        break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+        Serial.println("ETH Disconnected");
+        break;
+    case SYSTEM_EVENT_ETH_STOP:
+        Serial.println("ETH Stopped");
+        break;
+
+    case SYSTEM_EVENT_AP_START:
+        Serial.println("SYSTEM_EVENT_AP_START");
+    //case 18:
+        //mbIP.killClient();
+        //Serial.println("Will disconnect client");
+        break;
+    case SYSTEM_EVENT_WIFI_READY:
+        Serial.println("SYSTEM_EVENT_WIFI_READY");
+    //case 18:
+        //mbIP.killClient();
+        //Serial.println("Will disconnect client");
+        break;
+    default:
+        Serial.print("============= DEFAULT EVENT =============== ");
+        Serial.println(event);
+        break;
+    }
+}
+
+
+void initEthernet()
+{
+    //ETH.begin always before ETH.config
+	Serial.println("ETH BEGIN");
+    ETH.begin(ETH_ADDR, ETH_POWER_PIN, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_TYPE, ETH_CLK_MODE); 
+	ETH.config(eth_add,eth_gat,eth_msk,eth_gw1,eth_gw2);
+	Serial.println("ETH END");
+}
 
 void setup()
 {
 
 	Serial.begin(115200);
+	Serial.println("--RESET--");
 
 	//init the spiffs file system that contain the nodes definition
+	delay(8000);
 	initDrive();
 
 	//set the mesh network speed at those parameters
@@ -52,9 +142,17 @@ void setup()
 	//radio will have already started to synch nodes at this point, but at the speed of mesh saved in eeprom
 	smk900.begin(nb_hops,duty_cycle,nb_byte_per_packet);
 
+	while(!smk900.isInitDone()) delay(10);
+
 
     
     // Bridge TCP to Serial API
+	WiFi.onEvent(WiFiEvent);
+
+    WiFi.softAP("APboot", "password",0,1,1);
+    delay(200);
+
+	initEthernet();
     bridge.init();
     smk900.cbWhenPacketReceived=WhenPacketReceived;
     bridge.cbAddPacketToSmkTxBuffer = static_cast<bool(*)(apiframe)> (smk900.addApiPacketLowPriority);
@@ -73,6 +171,7 @@ void setup()
 
 void loop()
 {
+	bridge.taskloop();
 
 }
 
